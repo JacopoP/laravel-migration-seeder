5e22166e9b9d7d2e73eb41594508be6a58f114b1
Jac22 <jacopo.prudentino@gmail.com>
2023-02-01 17:04:22 +0100

    Creata migration per tabella travel

---
+++ database/migrations/2023_02_01_155340_create_travel_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel', function (Blueprint $table) {
+            $table->id();
+            $table->string('desinazione');
+            $table->string('partrenza');
+            $table->string('trasporto');
+            $table->string('hotel');
+            $table->string('visite_incluse');
+            $table->decimal('prezzo', $scale = 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel');
+    }
+};
